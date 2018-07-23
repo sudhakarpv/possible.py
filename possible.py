f0b57ee7f77c28eea8c1efec6706d61de716d5bd
pvs <40256682+sudhakarpv@users.noreply.github.com>
2018-07-23 12:19:58 +0530

    Update possible.py

--- possible.py
+++ possible.py
@@ -2,12 +2,21 @@
 def main():
     pass
     n=input()
-    user=input().split()
-    k=list(user)
-    k.sort()
-    q=k[::-1]
-    l=''.join(q)
-    print(l)
-
+    c=[]
+    temp=0
+    user=list(map(int,input().split()))
+    user.sort()
+    str_rev=user[::-1]
+    for i in user :
+        if i==0:
+            c.append(i)
+        if (len(c)==len(user)):
+            temp=1
+        else:
+            out=str_rev
+    if (temp>0):
+        print(0)
+    else:
+        print(''.join(map(str,out)))
 if __name__ == '__main__':
     main()
