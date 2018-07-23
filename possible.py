a02c0ea19b48907dfb62b7f21d22203356ce0f4c
pvs <40256682+sudhakarpv@users.noreply.github.com>
2018-07-23 11:42:48 +0530

    Update possible.py

--- possible.py
+++ possible.py
@@ -1,7 +1,13 @@
 # possible.py
-user=input()
-k=list(user)
-k.sort()
-q=k[::-1]
-l=''.join(q)
-print(l)
+def main():
+    pass
+    n=input()
+    user=input().split()
+    k=list(user)
+    k.sort()
+    q=k[::-1]
+    l=''.join(q)
+    print(l)
+
+if __name__ == '__main__':
+    main()
